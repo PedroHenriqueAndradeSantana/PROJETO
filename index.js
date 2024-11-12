ae2b8622d730580eb9aaef77687ea4395efd8e26
Pedro <pandradesantana.2005@gmail.com>
2024-11-11 23:53:38 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const filmes = require('./filmes.json');
+const path = require('path');
+const app = express();
+const PORT = 8080;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/filmes', (req, res) => {
+    res.json(filmes);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor em execução em http://localhost:${PORT}`);
+});
